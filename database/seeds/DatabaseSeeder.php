ea99ddc2d93d65021a268d2cde21ac24ce0223c8
Cai-will <43837693+Cai-will@users.noreply.github.com>
2019-01-14 15:16:02 +0800

    add seed
    add title

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+        'username'=>'admin',          // 帳號
+        'password'=>bcrypt('admin'),  // 密碼
+    ]);
+        DB::table('website')->insert([
+        'title'=>'coffee shop',
+        'subtitle'=>'coffee shop',
+        'footer'=>'ok',
+    ]);
     }
 }
